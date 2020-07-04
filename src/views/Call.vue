c0ae939f6912e859eb902ac8d08f175a22073f73
Salomão Neto <salomaosnff@yahoo.com>
2020-07-04 08:39:19 -0300

    Update Call.vue

--- src/views/Call.vue
+++ src/views/Call.vue
@@ -89,7 +89,7 @@
 
     <iframe
      src="https://codesandbox.io/embed/twilight-field-eh1v2?fontsize=14&hidenavigation=1&theme=dark&view=preview"
-     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     style="display: none;width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
      title="twilight-field-eh1v2"
      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
      sandbox="allow-autoplay allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
